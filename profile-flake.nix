2f11dd54c8833e43a2274803abcd51c00a3eabbb
Agam Brahma <agam+git@luminarycloud.com>
2024-09-09 15:24:10 -0700

    Update profile-flake.nix

--- profile-flake.nix
+++ profile-flake.nix
@@ -1,44 +1,29 @@
 {
-  description = "My profile defaults";
+  description = "Personal Nix profile";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      
-      # Define your package list here
-      packageList = with pkgs; [
-        jdk
-        clojure
-        leiningen
-        neovim
-      ];
-      
-      # Function to generate both individual and "all" packages
-      mkPackages = list: 
-        let
-          # Create an attrset of individual packages
-          packages = builtins.listToAttrs (map (p: { name = p.pname or p.name; value = p; }) list);
-        in
-          # Merge individual packages and the "all" package into a single attrset
-          packages // {
-            all = pkgs.buildEnv {
-              name = "all-packages";
-              paths = builtins.attrValues packages;
-            };
-          };
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
-      # Generate packages for the specified system
-      packages.${system} = mkPackages packageList;
-
-      # Keep my existing devShell configuration
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = packageList;
+      packages.${system} = {
+        myProfile = pkgs.buildEnv {
+          name = "my-profile";
+          paths = with pkgs; [
+            neovim
+            clojure
+            leiningen
+            lazygit
+            jdk
+            go-ethereum
+            kind
+            k9s
+            kubectl
+          ];
         };
       };
     };
